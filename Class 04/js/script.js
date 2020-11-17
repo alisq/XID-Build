c961454a6205039c31ea71550399f9c8182aee61
alisq <ali.shamas.qadeer@gmail.com>
2020-11-17 17:17:30 -0500

    d

--- Class 04/js/script.js
+++ Class 04/js/script.js
@@ -1,13 +1,26 @@
+/*
+
+click on an element on your page to change the background color.
+
+click on an element on your page to make another element fade out.
+
+hover on an image on your page to replace it with another image
+
+*/
+
+
 //when you click on a div with the class "menu"
 $(".menu").click(function(){
 
 
 
-	if ( $(this).hasClass("open") ) { 	//this menu is already open
-		$(this).removeClass("open")		//remove the "open" class
-	} else {		//otherwise it isn't open
-		$(this).addClass("open")	//add the "open" class
-	}
+	$(this).toggleClass("open");
+
+	// if ( $(this).hasClass("open") ) { 	//this menu is already open
+	// 	$(this).removeClass("open")		//remove the "open" class
+	// } else {		//otherwise it isn't open
+	// 	$(this).addClass("open")	//add the "open" class
+	// }
 
 	
 })
