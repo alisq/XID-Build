13d29a90a76bdae62d4b529acd4648856000d5e2
alisq <ali.shamas.qadeer@gmail.com>
2020-11-24 14:59:59 -0500

    added clas 05.

---
+++ Class-05/js/main.js
@@ -0,0 +1,49 @@
+
+
+
+/*
+
+alert to console.log
+
+menu click (add open class)
+
+double hover (change another... css then shapeshift class.)
+
+scroll  rotate element (calculate var) add var to menu.
+
+make array variable of people, then random. then bottom.
+
+
+Rima Al Moman, 
+Ben Bonsu,
+Edward Chan,
+Jason Cheng,
+Sydney Cooling-Sturges,
+Nikita Dorokhov,
+Max Fine,
+Oleg Gorlenko,
+Hadas Green,
+Wenjing Hong,
+Jasmine Khnanishoo,
+Henry McGuire,
+Ajay Naraindas,
+Ayomide Olagbemi,
+Justine Orbovic,
+Xuejun Pei,
+Theo Richardson,
+Connor Shopa,
+Chance Snow-Shermet,
+Ge Song,
+Schylar Van Den Helm,
+Tim Yim,
+Anna Lucia Zablah, 
+Jun Zhang,Shu 
+Haocheng Zhao,
+Wenyue Zheng,
+Eric Zhou
+
+/*
+
+
+
+
